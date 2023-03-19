c36ab953427020f211940f0714e088d7b2185b00
Oksan27 <omarkanoun16@gmail.com>
2023-03-19 04:10:53 -0700

    commit

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 /**
  * main - Prints the alphabet without q and e.
  *
@@ -9,14 +8,11 @@
  */
 int main(void)
 {
-	int i;
-
-	for (i = 97; i < 123; i++)
+	char alp[24] = "abcdfghijklmnoprstuvwxyz";
+             int i ;
+	for (i = 0; i <24; i++)
 	{
-		if (i != 101 && i != 113)
-		{
-			putchar(i);
-		}
+		putchar(alp[i]);
 	}
 	putchar('\n');
 	return (0);
